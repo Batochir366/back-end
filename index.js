aef7471a0b6e4c7a35d023b278ab095d2477f5a5
24LP2674 24LP2674 <24LP2674@setups-iMac-2.local>
2025-04-08 14:23:31 +0800

    backend

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 import express, { json } from "express";
-
+import { v4 as uuidv4 } from "uuid";
 const port = 8000;
 const app = express();
 app.use(json());
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+let users = [];
+app.post("/user/create", (req, res) => {
+  const { username, gender, age, email } = req.body;
+  users.push({ username, gender, age, email, id: uuidv4() });
+
+  res.send({ sucess: true, message: "done" });
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.get("/user", (req, res) => {
-  const { username, password } = req.body;
+  const { id } = req.body;
+  const user = users.find((user) => {
+    return user.id === id;
+  });
 
-  if (username.length < 5) {
-    res
-      .status(400)
-      .send({
-        success: false,
-        message: "username 5 deesh !!",
-      })
-      .end();
-  }
+  res.send(user);
+});
 
-  if (password.lengths < 8) {
-    res
-      .status(400)
-      .send({
-        success: false,
-        message: "password 8 deesh baih ystoi !!",
-      })
-      .end();
-  }
+app.delete("/user/delete", (req, res) => {
+  const { id } = req.body;
+  users = users.filter((user) => {
+    return user.id !== id;
+  });
 
-  res.send({ succes: true });
-  console.log(req.body, "req");
+  res.send({
+    success: true,
+    message: "deleted succesfully",
+  });
+});
+
+app.put("/user/update", (req, res) => {
+  const { id, username, gender, age, email } = req.body;
+  users.map((value, _) => {
+    if (value.id === id) {
+      (value.username = username),
+        (value.gender = gender),
+        (value.age = age),
+        (value.email = email);
+    } else {
+      return value;
+    }
+  });
+
+  res.send({
+    success: true,
+    message: "updated",
+  });
 });
 
 app.listen(port, () => {
